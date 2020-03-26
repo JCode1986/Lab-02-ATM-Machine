8d8c4bcc8c30c08f6f74ddc7806e9da2ffbbef58
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-03-26 01:56:31 -0700

    readme and tests added

--- ATM-Test/UnitTest1.cs
+++ ATM-Test/UnitTest1.cs
@@ -1,3 +1,4 @@
+using ATM_Machine;
 using System;
 using Xunit;
 
@@ -8,7 +9,44 @@ namespace ATM_Test
         [Fact]
         public void Test1()
         {
+            Assert.Equal(5000, Program.ViewBalance(Program.Balance = 5000));
+        }
 
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(50009, Program.ViewBalance(Program.Balance = 50009));
+        }
+
+        [Theory]
+        [InlineData(200, 4800)]
+        [InlineData(5000, 0)]
+        [InlineData(2500, 2500)]
+        [InlineData(4999, 1)]
+        public void Test3(decimal num, decimal expected)
+        {
+            Program.Balance = 5000;
+            decimal actual = Program.Withdraw(num);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(200, 5200)]
+        [InlineData(5000, 10000)]
+        [InlineData(2500, 7500)]
+        [InlineData(4999, 9999)]
+        public void Test4(decimal num, decimal expected)
+        {
+            Program.Balance = 5000;
+            decimal actual = Program.Deposit(num);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ThrowExceptionIfWithdrawingMoreThanBalance()
+        {
+            Program.Balance = 20;
+            Assert.Throws<Exception>(() => Program.Withdraw(9999999));
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Lab-02-ATM-Machine
+# Console Application ATM Machine
+
+## Lab 02 - Unit Testing
+
+*Author: Joseph Hangarter*
+
+----
+
+## Description
+Basic ATM machine where a user can deposit, withdraw, or view account.
+* To view balance - press '1'
+* To withdraw - press '2'
+* To depost - press '3'
+
+---
+
+### Getting Started
+Clone this repository to your local machine.
+
+```
+$ git clone [https://github.com/JCode1986/Lab-02-ATM-Machine.git]
+```
+
+### To run the program from Visual Studio:
+Select ```File``` -> ```Open``` -> ```Project/Solution```
+
+Next navigate to the location you cloned the Repository.
+
+Double click on the ```ATM-Machine``` directory.
+
+Then select and open ```ATM-Machine.sln```
+
+
+### Visuals
+***[Add screenshots of your application in action]***
+
+#### Application Start
+![Image 1](assets/start.png)
+#### Using the Application
+![Image 2](assets/menu.png)
